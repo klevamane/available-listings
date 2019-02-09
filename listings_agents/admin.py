d6ecadce638123c8395b6b4e100f9b2d75f00c6c
Onengiye Richard <klevamane@gmail.com>
2019-02-09 17:52:14 +0100

    feat(admin) update admin panel functionality
    - add search, filter, list link functionalities to models in the admin panel
    - add functionality to the listing model in the admin panel
    - add functionalities to the Agent(listings_agent) model in the admin panel

--- listings_agents/admin.py
+++ listings_agents/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from .models import Agent
 
+# set up the model admin
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'phone')
+    list_per_page = 10
+
+
 # Register your models here.
-admin.site.register(Agent)
+admin.site.register(Agent, AgentAdmin)
