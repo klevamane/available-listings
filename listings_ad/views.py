36d734d766ded1fb97638efb84e0d2a74ea2c417
Onengiye Richard <klevamane@gmail.com>
2019-02-11 13:25:24 +0100

    ft(listings) update the index and about page listing with dynamic data
    - update the index page listing with dynamic data
    - update the about page to return dynamic data for agent of the month
    - update single list page with dynamic data
    - implement searching of listings by keyword, bedrooms, city and/or state

--- listings_ad/views.py
+++ listings_ad/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Listing
-
+from listings_ad.search_choices import price_choices, bedroom_choices, state_choices
 
 # Create your views here.
 
@@ -22,8 +22,41 @@ def index(request):
 
 # ensure to pass the id if passed in the urls.py
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    single_listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        "listing": single_listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html') # templates/listings/search.html
+    query_list = Listing.objects.order_by('-list_date')
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_list = query_list.filter(description__icontains=keywords) # check __icontains in the documentation
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_list = query_list.filter(city__iexact=city)
+
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_list = query_list.filter(state__iexact=state)
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_list = query_list.filter(bedrooms__lte=bedrooms) #lte is "less than or equal to"
+
+    context = {
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+        "state_choices": state_choices,
+        "listings": query_list,
+        "values": request.GET # use this for preserving the search keywords in the front end eg. value= {{values.bathrom}}
+
+    }
+    return render(request, 'listings/search.html', context) # templates/listings/search.html
--- listings_agents/admin.py
+++ listings_agents/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 
 from .models import Agent
 
+
 # set up the model admin
 class AgentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email')
+
+    list_display = ('id', 'name', 'email', 'is_mvp')
     list_display_links = ('id', 'name')
+    list_editable = ('is_mvp',)
     search_fields = ('name', 'email', 'phone')
     list_per_page = 10
 
